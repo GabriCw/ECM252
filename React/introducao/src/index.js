f4cf97d329045bd7c2dd505f8cd4e7f6e315d48f
GabriCw <gabricouto2305@gmail.com>
2023-04-01 10:55:48 -0300

    Continuação REACT

--- React/introducao/src/index.js
+++ React/introducao/src/index.js
@@ -1,10 +1,40 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import './style.css'
 
 //JSX: JavaScript Extension (XML)
 //Função no React debe começãr com letra maiúscula 
 const App = () => {
-  return <div>Meu primeiro componente React</div>
+  const estilosBotao = {marginTop: 12, paddingTop: 8, paddingBottom: 8, backgroundColor: "blueviolet", color: "white", border: "none", width: "100%", borderRadius: 8}
+
+  const textoDoRotulo = "Nome:"
+
+  const obterTextoDoBotao = () => "Enviar"
+
+  const onClick = () => alert("Clicou")
+
+  return <div style={{margin: "auto", width: 768, backgroundColor: "#EEE", padding: 12, borderRadius: 8}}>
+
+    <label  
+    className='rotulo'
+    htmlFor='nome' 
+    style={{display: "block", marginBottom: 4}}>
+      {textoDoRotulo}
+    </label>
+
+    <input 
+    type='text' 
+    id='nome' 
+    style={{paddingTop: 8, borderStyle: "hidden", outline: "none", width: "100%", borderRadius: 8, boxSizing: "border-box"}} />
+
+    <button 
+    style={estilosBotao}
+    //onClick={onClick} ou
+    onClick={() => onClick()}> 
+      {obterTextoDoBotao()}
+    </button>
+
+  </div>
 }
 
 ReactDOM.render(
