71c0f4bfdebf78fe2f34589d6e588649d9983255
Gabriel <gabricouto2305@gmail.com>
2023-10-07 11:10:45 -0300

    Inserindo novas imagens a cada ButtonAdd click

--- flutter/exibe_imagens/lib/src/app.dart
+++ flutter/exibe_imagens/lib/src/app.dart
@@ -1,66 +1,58 @@
-import 'package:exibe_imagens/src/models/image_model.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
+import 'models/image_model.dart';
+import 'widgets/image_list.dart';
 
-class App extends StatefulWidget{
+class App extends StatefulWidget {
   @override
-  State <App> createState() {
+  State<App> createState() {
     return AppState();
   }
 }
 
-class AppState extends State <App>{
+class AppState extends State<App> {
+  int paginaAtual = 1;
+  List<ImageModel> imagens = [];
 
-  void obterImagem(){
-
-    //1. Montar a URL incluindo: host, recurso e parâmetros de query (query, page, per_page)
-
-    final url = Uri.https(
+  void obterImagem() async {
+    var url = Uri.https(
       'api.pexels.com',
       '/v1/search',
-      {'query': 'cats', 'page': '$numeroImagens', 'per_page': '1'}
-      );
-
-    //2. Montar um objeto Request com: método get e a URL contruida anteriormente
-
-    final req = http.Request('get', url);
-
-    //3. Adicionar o header Authorization à requisição, associado à nossa chave pexels
-
-    req.headers.addAll({'Authorization': '5vzx5FZnoKtLYReQQgoxOalCwaBowEngkTNxyaxGjBeEbsanKQfQYFs5'});
-
-    //4. Enviar a requisição
-
-    req.send().then((result){
-      http.Response.fromStream(result).then((response){
-        // print(response.body);
-        //json(String) --> mapa<String, dynamic> --> ImageModel
-        final decodedJSON = json.decode(response.body);
-        final imagem = ImageModel.fromJSON(decodedJSON);
-        print(imagem);
-        setState(() {
-          numeroImagens++;
-        });
+      {'query': 'people', 'page': '$paginaAtual', 'per_page': '1'},
+    );
+    var req = http.Request('get', url);
+    req.headers.addAll(
+      {
+        'Authorization': '5vzx5FZnoKtLYReQQgoxOalCwaBowEngkTNxyaxGjBeEbsanKQfQYFs5',
+      },
+    );
+    final result = await req.send();
+    if (result.statusCode == 200) {
+      final response = await http.Response.fromStream(result);
+      var decodedJSON = json.decode(response.body);
+      var imagem = ImageModel.fromJSON(decodedJSON);
+      setState(() {
+        paginaAtual++;
+        imagens.add(imagem);
       });
-    });
+    } else {
+      print('Falhou');
+    }
   }
 
-  int numeroImagens = 1;
-
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Minhas imagens'),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: obterImagem,
-          child: Icon(Icons.add_a_photo),
-        ),
-        body: Text('Imagens: $numeroImagens'),
+        home: Scaffold(
+      appBar: AppBar(
+        title: const Text("Minhas Imagens"),
       ),
-    );
+      floatingActionButton: FloatingActionButton(
+        child: const Icon(Icons.add),
+        onPressed: obterImagem,
+      ),
+      body: ImageList(imagens),
+    ));
   }
 }
--- flutter/exibe_imagens/lib/src/models/image_model.dart
+++ flutter/exibe_imagens/lib/src/models/image_model.dart
@@ -1,25 +1,19 @@
-class ImageModel{
+import 'package:flutter/material.dart';
+
+class ImageModel {
   late String url;
   late String alt;
 
-  // ImageModel(String url, String alt){
-  //   this.url = url;
-  //   this.alt = alt;
-  // }
   ImageModel(this.url, this.alt);
 
-  //construtores nomeados
-  ImageModel.fromJSON(Map<String, dynamic> decodedJSON){
-    url = decodedJSON['photos'][0]['src']['portrait'];
+  ImageModel.fromJSON(Map <String, dynamic> decodedJSON) {
+    url = decodedJSON['photos'][0]['src']['medium'];
     alt = decodedJSON['photos'][0]['alt'];
   }
 
   @override
   String toString() {
-    return 'url: $url, alt: $alt';
+    return 'url:$url, alt:$alt';
   }
-
-  //json(String) --> mapa<String, dynamic> --> ImageModel
-
 
 }
