8a86bc5462333f02ce017ab3f4ac5f9ca23071c9
Gabriel <gabricouto2305@gmail.com>
2023-10-07 12:56:20 -0300

    App responsivo feito

--- flutter/responsividade/lib/main.dart
+++ flutter/responsividade/lib/main.dart
@@ -22,9 +22,80 @@ class MeuLayoutResponsivo extends StatelessWidget{
       appBar: AppBar(title: Text("App responsiva")),
       body: LayoutBuilder(
         builder:(BuildContext context, BoxConstraints constraints){
-          return Text(constraints.maxWidth <= 576 ? 'Pequena' : 'Grande');
+          return constraints.maxWidth <= 576 ? MobileLayout() : WebLayout();
         }
       )
+    );
+  }
+}
+
+class MobileLayout extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return MobileLayoutState();
+  }
+}
+
+class MobileLayoutState extends State<MobileLayout> with SingleTickerProviderStateMixin{
+
+  TabController? tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    tabController = TabController(length: 2, vsync: this);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    //div class d-flex flex-column
+    return Column(
+      children: [
+        TabBar(
+          labelColor: Colors.black,
+          controller: tabController,
+          tabs: [Tab(text: "Aba 1"), Tab(text: "Aba 2")]
+          ),
+          Expanded(child: TabBarView(
+            controller: tabController,
+            children: [
+              Center(child: Text("Conteúdo da aba 1")),
+              Center(child: Text("Conteúdo da aba 2"))
+            ],
+          ))
+      ],
+    );
+  }
+}
+
+class WebLayout extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    return WebLayoutState();
+  }
+}
+
+class WebLayoutState extends State<WebLayout>{
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Expanded(
+          child: Card(
+            child: Center(
+              child: Text("Conteúdo da aba 1"),
+            )
+            )
+          ),
+        Expanded(
+          child: Card(
+            child: Center(
+              child: Text("Conteúdo da aba 2"),
+            )
+            )
+        )
+      ],
     );
   }
 }
